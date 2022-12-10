cd4543794f399e503b82198c6f31061f449c22aa
kjothen <kieran.othen@mac.com>
2022-12-10 09:56:09 +0000

    format day 10

--- src/aoc-2022/day_10.clj
+++ src/aoc-2022/day_10.clj
@@ -2,36 +2,40 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]
             [com.rpl.specter :refer [ALL LAST setval transform]]))
+
 (defn process
   [instruction-lines fn-accumulator initial-value]
   (loop [x 1
-         cycle-count 1
+         cycle 1
          result initial-value
          instructions (string/split-lines instruction-lines)]
     (if-not instructions
       result
       (let [[cmd arg] (string/split (first instructions) #" ")]
-        (recur (condp = cmd "addx" (+ x (parse-long arg)) "noop" x)
-               (condp = cmd "addx" (+ 2 cycle-count) "noop" (+ 1 cycle-count))
-               (fn-accumulator result
-                               cmd
-                               (when (some? arg) (parse-long arg))
-                               x
-                               cycle-count)
-               (next instructions))))))
+        (recur
+         (condp = cmd "addx" (+ x (parse-long arg)) "noop" x)
+         (condp = cmd "addx" (+ 2 cycle) "noop" (+ 1 cycle))
+         (fn-accumulator result cmd (when (some? arg) (parse-long arg)) x cycle)
+         (next instructions))))))
+
 (defn signal-strengths
-  [strength-cycles strengths cmd arg x cycle-count]
-  (cond-> strengths
-    (contains? strength-cycles (inc cycle-count)) (conj (* (inc cycle-count) x))
-    (and (= "addx" cmd) (contains? strength-cycles (+ 2 cycle-count)))
-    (conj (* (+ 2 cycle-count) (+ x arg)))))
+  [strength-cycles strengths cmd arg x cycle]
+  (let [next-cycle (inc cycle)
+        next-next-cycle (inc next-cycle)]
+    (cond-> strengths
+      (contains? strength-cycles next-cycle) (conj (* next-cycle x))
+      (and (= cmd "addx") (contains? strength-cycles next-next-cycle))
+      (conj (* next-next-cycle (+ x arg))))))
+
 (defn sprite-position [x] #{(dec x) x (inc x)})
+
 (defn pixel-position [cycle width] [(dec (mod cycle width)) (quot cycle width)])
+
 (defn cathode-ray-tube
-  [width display cmd arg x cycle-count]
-  (let [[xpos ypos] (pixel-position cycle-count width)
-        [next-xpos next-ypos] (pixel-position (+ 1 cycle-count) width)
-        [next-next-xpos next-next-ypos] (pixel-position (+ 2 cycle-count) width)
+  [width display _ arg x cycle]
+  (let [[xpos ypos] (pixel-position cycle width)
+        [next-xpos next-ypos] (pixel-position (+ 1 cycle) width)
+        [next-next-xpos next-next-ypos] (pixel-position (+ 2 cycle) width)
         sprite-pos (sprite-position x)
         next-next-sprite-pos (when (some? arg) (sprite-position (+ x arg)))]
     (cond->> display
@@ -43,9 +47,11 @@
              (setval [next-next-ypos next-next-xpos] \#))))
 
 (defn render-display [display] (doseq [x display] (println (apply str x))))
+
 (defn stringify-display
   [display]
   (string/join \newline (map (partial apply str) display)))
+
 (defn result
   [data]
   {:part-1
@@ -55,6 +61,7 @@
                  display (vec (repeat height (vec (repeat width \.))))]
              (stringify-display
               (process data (partial cathode-ray-tube width) display)))})
+
 (let
   [test-data (slurp (io/resource "aoc-2022/10/test-input.dat"))
    input-data (slurp (io/resource "aoc-2022/10/input.dat"))
