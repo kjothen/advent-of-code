5111e66eee3675300a140e4d82bc70b684688558
Kieran Othen <kieran.othen@mac.com>
2023-12-02 18:15:58 +0000

    Day 2

--- src/aoc-2023/day_02.clj
+++ src/aoc-2023/day_02.clj
@@ -4,22 +4,65 @@
     [clojure.string :as str]
     [clojure.test :refer [deftest is testing]]))
 
-(defn part-one [s] s)
+(defn colour->freqs
+  [turn-str]
+  (reduce (fn [result colour-freq]
+            (let [[freq colour] (str/split colour-freq #" ")]
+              (assoc result colour (parse-long freq))))
+          {}
+          (str/split turn-str #", ")))
 
-(defn part-two [s] s)
+(defn id+turns
+  [game-str]
+  (let [regex #"^Game ([0-9]+)\: (.*)$"
+        id+turns (rest (first (re-seq regex game-str)))
+        id (parse-long (first id+turns))
+        turns (mapv colour->freqs (str/split (second id+turns) #"; "))]
+    [id turns]))
+
+(defn turns-possible?
+  [turns limits]
+  (reduce (fn [_ turn]
+            (if (false? (reduce-kv (fn [_ colour freq]
+                                     (if (> freq (get limits colour))
+                                       (reduced false)
+                                       true))
+                                   true
+                                   turn))
+              (reduced false)
+              true))
+          true
+          turns))
+
+(defn turns-power [turns] (apply * (vals (apply merge-with max turns))))
+
+(defn part-one
+  [s]
+  (let [games (map id+turns (str/split-lines s))
+        limits {"red" 12 "green" 13 "blue" 14}]
+    (reduce (fn [result [id turns]]
+              (prn id)
+              (if (turns-possible? turns limits) (+ id result) result))
+            0
+            games)))
+
+(defn part-two
+  [s]
+  (let [games (map id+turns (str/split-lines s))]
+    (apply + (map turns-power (map second games)))))
 
 (deftest day-02
   (testing "--- Part One ---"
     (testing "Example Input"
       (let [data (slurp (io/resource "aoc-2023/02/example.dat"))]
-        (is (= data (part-one data)))))
+        (is (= 8 (part-one data)))))
     (testing "Puzzle Input"
       (let [data (slurp (io/resource "aoc-2023/02/input.dat"))]
-        (is (= data (part-one data))))))
+        (is (= 3059 (part-one data))))))
   (testing "--- Part Two ---"
     (testing "Example Input"
       (let [data (slurp (io/resource "aoc-2023/02/example.dat"))]
-        (is (= data (part-two data)))))
+        (is (= 2286 (part-two data)))))
     (testing "Puzzle Input"
       (let [data (slurp (io/resource "aoc-2023/02/input.dat"))]
-        (is (= data (part-two data)))))))
+        (is (= 65371 (part-two data)))))))
