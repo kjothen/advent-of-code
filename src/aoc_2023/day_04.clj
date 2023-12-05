047281f48759bac4d6ca557d04097187e752a478
kieran-othen_jpmc <kieran.othen@chase.io>
2023-12-05 11:01:16 +0000

    Day 4 Complete

--- src/aoc_2023/day_04.clj
+++ src/aoc_2023/day_04.clj
@@ -12,8 +12,7 @@
 
 (defn card-matches
   [line]
-  (let [[_ _ numbers-str] (re-find #"^Card(?:[ ]+)([0-9]+)\: (.*)$" line)
-        [winning-numbers-str card-numbers-str] (str/split numbers-str #"\|")
+  (let [[_ winning-numbers-str card-numbers-str] (str/split line #":|\|")
         card-numbers (number-strs->numbers card-numbers-str)
         winning-numbers (number-strs->numbers winning-numbers-str)]
     (count (clojure.set/intersection card-numbers winning-numbers))))
@@ -23,36 +22,26 @@
   (let [matches (card-matches line)]
     (if (pos? matches) (int (math/pow 2 (dec matches))) 0)))
 
-(defn part-one [s] (apply + (map card-worth (str/split-lines s))))
-
-(defn part-two
-  [s]
-  (let [matches (map card-matches (str/split-lines s))
+(defn card-wins
+  [lines]
+  (let [matches (map card-matches lines)
         idx-max (dec (count matches))]
-    (loop [winnings {}
+    (loop [cards (vec (repeat (inc idx-max) 1))
            idx 0]
-      (if (> idx idx-max)
-        (apply + (vals winnings))
-        (let [wins (nth matches idx)]
-          (prn winnings)
+      (if (= idx idx-max)
+        cards
+        (let [wins (nth matches idx)
+              copies (nth cards idx)
+              winning-cards (range (inc idx) (+ (inc idx) wins))]
           (recur (reduce (fn [result win-idx]
-                           (update-in result
-                                      (str win-idx)
-                                      (fn [curr] (inc (or curr 0)))))
-                         winnings
-                         (when (pos? wins)
-                           (range (inc idx) (min idx-max (+ (inc idx) wins)))))
+                           (update result win-idx (partial + copies)))
+                         cards
+                         winning-cards)
                  (inc idx)))))))
 
-(comment
-  (def data (slurp (io/resource "aoc-2023/04/example.dat")))
-  (def lines (str/split-lines data))
-  (apply + (map card-worth lines))
+(defn part-one [s] (apply + (map card-worth (str/split-lines s))))
 
-  (part-two data)
-
-  ;
-)
+(defn part-two [s] (apply + (card-wins (str/split-lines s))))
 
 (deftest day-04
   (testing "Part One - Example"
@@ -63,7 +52,7 @@
       (is (= 19135 (part-one data)))))
   (testing "Part Two - Example"
     (let [data (slurp (io/resource "aoc-2023/04/example.dat"))]
-      (is (= data (part-two data)))))
+      (is (= 30 (part-two data)))))
   (testing "Part Two - Puzzle"
     (let [data (slurp (io/resource "aoc-2023/04/input.dat"))]
-      (is (= data (part-two data))))))
+      (is (= 5704953 (part-two data))))))
